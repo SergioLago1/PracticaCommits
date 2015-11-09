071ef9531cee0ef94c7b4fed96927aa55a714a92
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2015-11-09 10:04:43 +0100

    Commit1

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -34,6 +34,9 @@ package com.toy.anagrams.lib;
 /**
  * Implementation of the logic for the Anagram Game application.
  */
+/*Comentario
+
+*/
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
